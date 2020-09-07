a14625edd1b1334fb431b7a17d3e8519e60dcf8c
Sidhanth Kafley <skafley@uvm.edu>
2020-09-07 00:39:49 -0400

    Just got deliverable 1, step 50 working

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,15 +1,12 @@
 var controllerOptions = {};
 
-var i = 0;
-
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
 Leap.loop(controllerOptions, function(frame)
 {
+  clear();
   var randomInteger = Math.floor(Math.random()*2) - 1;
   circle(x+randomInteger, y, 100);
-  console.log(i);
-  i++;
 }
 );
