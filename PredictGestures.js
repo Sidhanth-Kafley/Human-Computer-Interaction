c00c294f35bbf04dd49b7b1f464ed98a575b8eb6
Sidhanth Kafley <skafley@uvm.edu>
2020-10-10 17:12:28 -0400

    just got deliverable 6, step 3 working

--- PredictGestures.js
+++ PredictGestures.js
@@ -3,36 +3,36 @@ var predictedClassLabels = nj.zeros([150]);
 const knnClassifier = ml5.KNNClassifier();
 var testingSampleIndex = 0;
 var trainingCompleted = false;
+var controllerOptions = {};
 
-
-Leap.loop(controllerOptions, function(frame){
+Leap.loop(controllerOptions, function(frame) {
   clear();
-  if(trainingCompleted === false){
+  if (trainingCompleted === false) {
     Train();
     trainingCompleted = true;
   }
   Test();
 });
 
-function Train(){
-  for(var i=0; i<trainX.shape[3]; i++){
-    var features = trainX.pick(null,null,null,i).reshape(1,120).tolist();
-    var features1 = trainY.pick(null,null,null,i).reshape(1,120).tolist();
-    knnClassifier.addExample(features,3);
-    knnClassifier.addExample(features1,4);
+function Train() {
+  for (var i = 0; i < trainX.shape[3]; i++) {
+    var features = trainX.pick(null, null, null, i).reshape(1, 120).tolist();
+    var features1 = trainY.pick(null, null, null, i).reshape(1, 120).tolist();
+    knnClassifier.addExample(features, 3);
+    knnClassifier.addExample(features1, 4);
   }
 }
 
-function Test(){
-  var currentTestingSample = test.pick(null,null,null,testingSampleIndex).reshape(1,120);
+function Test() {
+  var currentTestingSample = test.pick(null, null, null, testingSampleIndex).reshape(1, 120);
   var predictedLabel = knnClassifier.classify(currentTestingSample.tolist(), GotResults);
   console.log(testingSampleIndex + "-----" + predictedClassLabels.get(testingSampleIndex));
 
 }
 
-function GotResults(err, result){
-  testingSampleIndex+=1;
-  if(testingSampleIndex>test.shape[3]-1){
+function GotResults(err, result) {
+  testingSampleIndex += 1;
+  if (testingSampleIndex > test.shape[3] - 1) {
     testingSampleIndex = 0;
   }
   predictedClassLabels.set(testingSampleIndex, parseInt(result.label));
