096b7960194a0d32672be766878c242d18eecacb
Sidhanth Kafley <skafley@uvm.edu>
2020-10-10 17:43:58 -0400

    just got deliverable 6, step 4 working

--- PredictGestures.js
+++ PredictGestures.js
@@ -11,8 +11,89 @@ Leap.loop(controllerOptions, function(frame) {
     Train();
     trainingCompleted = true;
   }
+  HandleFrame(frame);
   Test();
 });
+
+function HandleFrame(frame) {
+  if (frame.hands.length === 1 || frame.hands.length === 2) {
+    var hand = frame.hands[0];
+    var InteractionBox = frame.interactionBox;
+    HandleHand(hand, frame, InteractionBox);
+  }
+}
+
+function HandleHand(hand, frame, InteractionBox) {
+  var finger = hand.fingers;
+  for (var j = 3; j >= 0; j--) {
+    for (var i = 0; i < finger.length; i++) {
+      var bone = finger[i].bones;
+      var weight = 1;
+      var fingerIndex = finger[i].type;
+      HandleBone(j, bone, weight, frame, fingerIndex, InteractionBox);
+    }
+  }
+}
+
+function HandleBone(j, bone, weight, frame, fingerIndex, InteractionBox) {
+
+  var normalizePrevJoint = InteractionBox.normalizePoint(bone[j].prevJoint, true);
+  var normalizedNextJoint = InteractionBox.normalizePoint(bone[j].nextJoint, true);
+
+  var newTipX = normalizedNextJoint[0];
+  var newTipY = normalizedNextJoint[1];
+  var z = normalizedNextJoint[2];
+
+  var newBaseX = normalizePrevJoint[0];
+  var newBaseY = normalizePrevJoint[1];
+  var z1 = normalizePrevJoint[2];
+
+  var boneIndex = bone[j].type;
+
+  // framesOfData.set(fingerIndex,boneIndex,0,currrentSample,newBaseX);
+  // framesOfData.set(fingerIndex,boneIndex,1,currrentSample,newBaseY);
+  // framesOfData.set(fingerIndex,boneIndex,2,currrentSample,z1);
+  // framesOfData.set(fingerIndex,boneIndex,3,currrentSample,newTipX);
+  // framesOfData.set(fingerIndex,boneIndex,4,currrentSample,newTipY);
+  // framesOfData.set(fingerIndex,boneIndex,5,currrentSample,z);
+
+  var canvasX = window.innerWidth * normalizedNextJoint[0];
+  var canvasY = window.innerHeight * (1 - normalizedNextJoint[1]);
+
+  var canvasX1 = window.innerWidth * normalizePrevJoint[0];
+  var canvasY1 = window.innerHeight * (1 - normalizePrevJoint[1]);
+
+
+  var r, g, b = 0;
+
+  if (bone[j].type === 0) {
+    weight = 10;
+    r = 210;
+    g = 210;
+    b = 210;
+
+  } else if (bone[j].type === 1) {
+    weight = 7;
+    r = 169;
+    g = 169;
+    b = 169;
+
+  } else if (bone[j].type === 2) {
+    weight = 5;
+    r = 105;
+    g = 105;
+    b = 105;
+
+  } else if (bone[j].type === 3) {
+    weight = 2;
+    r = 0;
+    g = 0;
+    b = 0;
+  }
+  strokeWeight(weight);
+  stroke(r, g, b);
+  line(canvasX, canvasY, canvasX1, canvasY1);
+}
 
 function Train() {
   for (var i = 0; i < trainX.shape[3]; i++) {
