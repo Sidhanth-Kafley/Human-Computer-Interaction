3baa3e0d3843e432d966d31dd1bcac889221d2da
Sidhanth Kafley <skafley@uvm.edu>
2020-11-02 16:37:37 -0500

    Just got deliverable 8, step 30 working

--- PredictGestures.js
+++ PredictGestures.js
@@ -437,9 +437,15 @@ function SignIn(){
   username = document.getElementById('username').value;
   //console.log(username);
   var list = document.getElementById('users');
-  var item = document.createElement('li');
-  item.innerHTML = String(username);
-  list.appendChild(item);
+  if(IsNewUser(username,list)){
+    var item = document.createElement('li');
+    item.innerHTML = String(username);
+    list.appendChild(item);
+  }
   console.log(list.innerHTML);
   return false;
+}
+
+function IsNewUser(username,list){
+  var users = list.children;
 }
